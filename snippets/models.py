7c1fa794036bcf11caf03fd5dd34bf3ea4ea4d74
somya.hcode <somya.gupta@hcode.tech>
2024-11-07 12:04:01 +0530

    create model + ModelSerializer

---
+++ snippets/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default="")
+    lineos = models.BooleanField(default=False)
